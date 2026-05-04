f9992833f30ed46faeea644d63dddb34cf7e6156
eskildjuul <eskchr01@iba.dk>
2026-05-04 14:08:03 +0200

    Rettelser af JS færdig, så man kan bruge console

--- js/script.js
+++ js/script.js
@@ -1,20 +1,67 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const buttons = document.querySelectorAll(".btn");
-  const stages = document.querySelectorAll(".stage");
+let score = 0;
 
-  buttons.forEach((button) => {
-    button.addEventListener("click", () => {
-      const nextStageId = button.dataset.next;
+const btns = document.querySelectorAll(".stage .btn");
+const stages = document.querySelectorAll(".stage");
 
-      stages.forEach((stage) => {
-        stage.classList.remove("active");
-      });
+const showStage = (stageId) => {
+  for (const stage of stages) {
+    stage.classList.remove("active");
+  }
 
-      const nextStage = document.getElementById(nextStageId);
+  const nextStage = document.getElementById(stageId);
 
-      if (nextStage) {
-        nextStage.classList.add("active");
-      }
-    });
-  });
-});
+  if (nextStage) {
+    nextStage.classList.add("active");
+  }
+};
+
+const nextStage = (e) => {
+  const clickedBtn = e.target;
+  const stageId = clickedBtn.dataset.next;
+
+  switch (stageId) {
+    case "mail":
+      score = 0;
+      showStage("mail");
+      break;
+
+    case "check-sender":
+      score += 1;
+      showStage("check-sender");
+      break;
+
+    case "bad-ending":
+      score -= 1;
+      showStage("bad-ending");
+      break;
+
+    case "neutral-ending":
+      score += 0;
+      showStage("neutral-ending");
+      break;
+
+    case "okay-ending":
+      score += 1;
+      showStage("okay-ending");
+      break;
+
+    case "good-ending":
+      score += 2;
+      showStage("good-ending");
+      break;
+
+    case "start":
+      score = 0;
+      showStage("start");
+      break;
+
+    default:
+      console.log("Noget gik galt");
+  }
+
+  console.log("Score:", score);
+};
+
+for (const btn of btns) {
+  btn.addEventListener("click", nextStage);
+}
